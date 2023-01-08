ecb4ff5be33a559c59a445052c33e0a1d0f0ce62
Rituchoudhary67 <114747083+Rituchoudhary67@users.noreply.github.com>
2023-01-08 17:18:04 +0530

    Add files via upload

---
+++ Twitter/js/join.js
@@ -0,0 +1,7 @@
+const blackBtnSign = document.querySelector(".phone-email");
+console.log(blackBtnSign);
+
+blackBtnSign.addEventListener("click",(e) => {
+   document.querySelector(".join-sub-child").style.display = "none";
+})
+
